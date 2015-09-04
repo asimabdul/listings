5f7313c4ad7a20f2eba8df33cb2efcd3ab8a3682
Asim Abdul <asim@viralheat.com>
2015-09-03 20:07:30 -0700

    setting root route

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -8,12 +8,23 @@ module Api
       def pagination_links(model_scope, url, query_params)
         next_page = model_scope.current_page+1 if !model_scope.last_page?
         prev_page = model_scope.current_page-1 if !model_scope.first_page?
+
+        if !model_scope.first_page?
+          prev_page = model_scope.current_page-1
+          @first_page = "#{url}?#{query_params.merge(:page => 1).to_param}"
+        end
+
+        if !model_scope.last_page?
+          next_page = model_scope.current_page+1
+          @last_page = "#{url}?#{query_params.merge(:page => model_scope.total_pages).to_param}"
+        end
         
         next_url_params = query_params.merge(:page => next_page)
         prev_url_params = query_params.merge(:page => prev_page)
 
         @next_page = "#{url}?#{next_url_params.to_param}" if next_page
         @prev_page = "#{url}?#{prev_url_params.to_param}" if prev_page
+        
       end
       
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
 
+  root to: "api/v1/listings#index"
+
   namespace :api do
     namespace :v1 do
       resources :listings, only: [:index]
     end
   end
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
